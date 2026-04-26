4e0b54f3ae88bde3524ad0c6fc1c69a862d04654
Qk-Z-T <tamim640823@gmail.com>
2026-04-26 21:40:11 +0600

    Add initial analysis view component

---
+++ src/features/student/views/analysis.view.js
@@ -0,0 +1,8 @@
+// src/features/student/views/analysis.view.js
+
+import { getState } from '../../../core/state/store.js';
+
+export async function renderAnalysis(container) {
+    container.innerHTML = `<div class="p-5 pb-20"><h2 class="text-2xl font-bold mb-4">Analysis</h2><div class="p-6 text-slate-400">Coming soon...</div></div>`;
+    // future implementation
+}
