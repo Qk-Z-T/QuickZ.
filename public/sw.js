aa0cef9f21c4616639553c09cb9afa96ebdd3da2
Qk-Z-T <tamim640823@gmail.com>
2026-04-26 22:18:50 +0600

    Add service worker for caching static assets

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+// public/sw.js
+const CACHE_NAME = 'quickz-v2';
+const STATIC_ASSETS = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request).catch(() => caches.match('/offline.html'));
+        })
+    );
+});
